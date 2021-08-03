b7dc8186c26ea2ae186b536b81f131c288890f86
Phing An Hsiao <phinganhsiao@gmail.com>
2021-08-03 10:54:07 -0400

    tuesday AM

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-console.log('hooray');
-
-
+document.addEventListener('click' , function(e) {
+    console.log(e.target)
+})
 const h1 = document.querySelector('h1')
-h1.textContent = "changed it here"
+h1.textContent = 'My fantasy team'
 
-const divTag = document.querySelector('div')
-console.log(divTag)
+const topDisplay = document.createElement('img')
+topDisplay.id = 'banner'
+topDisplay.src = 'https://www.profeet.com/wp-content/uploads/sites/3/2019/06/banner.jpg'
+h1.appendChild(topDisplay)
 
 
-const img = document.querySelector('img')
-console.log(img)
-img.src = "https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"
+function playerCard(player) {
+    const div = document.createElement('div');
+    div.id = `${player.id}`
+    div.className = 'player-list'
 
+    const playerName = document.createElement('h2')
+    playerName.id = player.name
+
+    const img = document.createElement('img');
+    img.className = 'player-image'
+    img.src = player.img
+
+    const btn = document.createElement('button');
+    btn.className = 'selector'
+    btn.textContent = 'select'
+    btn.id = 'player-select'
+    btn.addEventListener('click', renderPlayer(player))
+
+    h1.append(div);
+    div.appendChild(playerName, img, btn);
+}
 
 const init = function() {
-    console.log("we got it")
 }
 
  document.addEventListener("DOMContentLoaded", init())
